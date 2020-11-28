2698a10ed391f5832ba22fcddf159fe71124bcca
Daniel <admg@ciencias.unam.mx>
2020-11-28 08:45:31 -0600

    Score of 1.08

--- prepararOutput.R
+++ prepararOutput.R
@@ -4,12 +4,17 @@ output <- data.frame(Id_Cliente = integer(), D1=integer(), D2=integer()
 clientes <- datos[,1]
 for (i in clientes) {
         output[i,1] <- i
-        output[i,2] <- sum(zona1[,1] == i)
-        output[i,3] <- sum(zona2[,1] == i)
-        output[i,4] <- sum(zona3[,1] == i)
-        output[i,5] <- sum(zona4[,1] == i)
-        output[i,6] <- sum(zona5[,1] == i)
-        output[i,7] <- sum(zona6[,1] == i)
+        output[i,2] <- sum(zona1_prueba[,1] == i)
+        output[i,3] <- sum(zona2_prueba[,1] == i)
+        output[i,4] <- sum(zona3_prueba[,1] == i)
+        output[i,5] <- sum(zona4_prueba[,1] == i)
+        output[i,6] <- sum(zona5_prueba[,1] == i)
+        output[i,7] <- sum(zona6_prueba[,1] == i)
+}
+
+for (i in 2:7) {
+        print(which(output[,i]>=2))
 }
 
 write.csv(output,"output.csv", row.names = FALSE)
+##Puntaje de 1.5 con m = 640
